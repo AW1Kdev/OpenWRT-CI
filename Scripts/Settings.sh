623454c1307cee774d035acf9a8eda15e52ca625
Doty The Cat <dotywrt@gmail.com>
2026-05-03 20:10:25 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+[ -z "$WRT_IP" ] && { echo "WRT_IP kosong!"; exit 1; }
 
-CFG_FILE="./package/base-files/files/bin/config_generate"
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
+echo "Set LAN IP: $WRT_IP"
 
-CFG_FILEX="./files/etc/config/network"
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILEX
+FLASH=$(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js" 2>/dev/null)
+[ -n "$FLASH" ] && sed -i "s/192\.168\.1\.1/$WRT_IP/g" "$FLASH"
+
+CFG="./package/base-files/files/bin/config_generate"
+[ -f "$CFG" ] && sed -i "s/ipaddr='192\.168\.1\.1'/ipaddr='$WRT_IP'/g" "$CFG"
+
+NET="./files/etc/config/network"
+if [ -f "$NET" ]; then
+  sed -i "/config interface 'lan'/,/config interface/ s/192\.168\.1\.1/$WRT_IP/" "$NET"
+fi
+
+echo "Done"
